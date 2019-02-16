592e7315e24a1435a2d7da29180e3d48f5ee61d6
Lucas Soares <lucas@mobile2you.com.br>
2019-02-16 15:05:33 -0200

    comments some pods

--- PersonalTraining/Views/Sica/SicaViewController.swift
+++ PersonalTraining/Views/Sica/SicaViewController.swift
@@ -10,10 +10,15 @@ import UIKit
 class SicaViewController: UIViewController {
 
     //MARK: - Outlets
-    @IBOutlet weak var indicator: UIView!
-    @IBOutlet weak var button1: GradientButton!
+    @IBOutlet weak var button1: CustomUIButton!
     @IBOutlet weak var button2: CustomUIButton!
-    @IBOutlet weak var leadingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var stackView: UIStackView!
+    
+    //Custom View
+    var indicator: UIView!
+    
+    //MARK: - Custom vars
+    var indicatorConstraints: [(_where: String, constraint: NSLayoutConstraint)] = []
     
     //MARK: - Life Cycle
     override func viewDidLoad() {
@@ -21,13 +26,31 @@ class SicaViewController: UIViewController {
         
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        indicator = UIView()
+        indicator.translatesAutoresizingMaskIntoConstraints = false
+        indicator.backgroundColor = .black
+        self.view.addSubview(indicator)
+        indicator.heightAnchor.constraint(equalToConstant: 2).isActive = true
+        indicator.leadingAnchor.constraint(equalTo: self.stackView.leadingAnchor, constant: 0).isActive = true
+        indicator.topAnchor.constraint(equalTo: self.stackView.bottomAnchor, constant: 0).isActive = true
+        indicator.widthAnchor.constraint(equalTo: self.button1.widthAnchor).isActive = true
+
+        
+        
+    }
+    
     
     //MARK: - Actions
     
     @IBAction func button2tap(_ sender: Any) {
-        if indicator.frame.origin.x == 0 {
-            self.leadingConstraint.constant = indicator.frame.size.width;
+        
+        if indicator.frame.origin.x == 8 {
+            print("Move indicator to button 2")
+            indicator.frame.origin.x = button1.frame.width + 16 // 16 = 8 leading da stack + 8 de espaço entre os botoes da stack
         }
+        
         UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 0.3, delay: 0.1, options: .curveEaseInOut, animations: {
             
             self.view.layoutIfNeeded()
@@ -37,9 +60,12 @@ class SicaViewController: UIViewController {
         
     }
     @IBAction func button1tap(_ sender: Any) {
-        if indicator.frame.origin.x == indicator.frame.size.width {
-            self.leadingConstraint.constant = 0;
+    
+        if Int(indicator.frame.origin.x) == Int(button1.frame.width + 16) {
+            print("Move indicator to button 1")
+            indicator.frame.origin.x = 8
         }
+        
         UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 0.3, delay: 0.1, options: .curveEaseInOut, animations: {
             
             self.view.layoutIfNeeded()
